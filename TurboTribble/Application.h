e536dbae41d39d934c3e1eee90e1ec1f12a9f589
FireAlfa <60975035+FireAlfa@users.noreply.github.com>
2021-09-27 08:48:56 +0200

    imgui doesn't crash

--- TurboTribble/Application.h
+++ TurboTribble/Application.h
@@ -13,7 +13,7 @@
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_sdl.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_opengl2.h"
 #include "SDL/include/SDL_opengl.h"
 
 
