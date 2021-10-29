46c7ec6e2cf3e0f7c5dd5c8bc500ed164cd6d760
Osvak <61042986+Osvak@users.noreply.github.com>
2021-10-29 10:04:49 +0200

    Update CMesh.h

--- TurboTribble/Engine/Source/CMesh.h
+++ TurboTribble/Engine/Source/CMesh.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include "Component.h"
+
+#include "MathGeoLib/include/Geometry/AABB.h"
+#include "MathGeoLib/include/Geometry/OBB.h"
+
+#include <vector>
+typedef unsigned int uint;
+
+class Mesh : Component
+{
+public:
+	Mesh() { Mesh::type = CompType::MESH; }
+	~Mesh();
+
+
+
+
+public:
+	uint id_index = 0; // index in VRAM
+	uint num_index = 0;
+	uint* index = nullptr;
+	uint id_vertex = 0; // unique vertex in VRAM
+	uint num_vertex = 0;
+	float* vertex = nullptr;
+
+};
